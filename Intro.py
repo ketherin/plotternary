2f43bf65023825e38a6c3c41b2853639ddcb8c73
ketherin <108742264+ketherin@users.noreply.github.com>
2025-05-23 22:35:31 -0300

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -5,17 +5,14 @@ st.set_page_config(layout="wide", page_title="Plotternary")
 
 theme = st_theme()
 
-_, container1, _ = st.columns([35, 70, 35])
-
 #Adding a Title
 if theme['backgroundColor'] == '#0e1117':
   st.sidebar.image("sidebar_light.png",  use_container_width=True)
-  container1.image("light_logo.png")
+  st.image("light_logo.png")
   
 else:
   st.sidebar.image("sidebar_dark.png",  use_container_width=True)
-  container1.image("dark_logo.png")
-  
+  st.image("dark_logo.png")
 
 st.text("")
 st.text("")
@@ -23,5 +20,5 @@ st.text("")
 
 video_data='https://www.youtube.com/watch?v=fxPOP65b_kE'
 
-_, container2, _ = st.columns([35, 70, 35])
-container2.video(data=video_data)
+_, container, _ = st.columns([35, 70, 35])
+container.video(data=video_data)
