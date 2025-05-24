299597ed3752d64b86619bd51ef14deec55ffd4e
ketherin <108742264+ketherin@users.noreply.github.com>
2025-05-23 22:24:20 -0300

    Intro.py

--- Intro.py
+++ Intro.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from streamlit_theme import st_theme
 
-theme = st_theme()
-
 st.set_page_config(layout="wide", page_title="Plotternary")
+
+theme = st_theme()
 
 _, container1, _ = st.columns([35, 70, 35])
 
