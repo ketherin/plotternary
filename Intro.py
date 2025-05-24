e78398b3ce3a02aa334a0cc9800ba0770a428bf3
ketherin <108742264+ketherin@users.noreply.github.com>
2025-05-23 22:15:16 -0300

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -8,7 +8,7 @@ theme = st_theme()
 _, container1, _ = st.columns([35, 70, 35])
 
 #Adding a Title
-if theme == dark:
+if theme == 'dark':
   st.sidebar.image("sidebar_light.png",  use_container_width=True)
   container1.image("light_logo.png")
 else:
